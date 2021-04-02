d0fbeb528064c68cdb6e75eaa058486b0cafc46b
shediao <xushediao1987@163.com>
2021-04-02 22:41:49 +0800

    adbauto 添加些帮助信息

--- adbauto.cc
+++ adbauto.cc
@@ -1,3 +1,7 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/resource.h>
+
 #include <iostream>
 #include <array>
 
@@ -5,6 +9,65 @@
 #include <boost/asio.hpp>
 #include <boost/algorithm/string/replace.hpp>
 #include <range/v3/all.hpp>
+
+#define RUN_IN_DAEMON 1
+
+#if RUN_IN_DAEMON
+bool g_daemon_process = false;
+#endif
+bool g_verbose = false;
+
+void die(int exit_code, std::string msg) {
+  std::cerr << msg << std::endl;
+  exit(exit_code);
+}
+void usage() {
+  std::cout << "adbauto [options] command [args...]\n";
+  std::cout << "\n";
+  std::cout << "options:\n";
+  std::cout << " -h:\n";
+  std::cout << " --help:    显示本帮助文档\n";
+  std::cout << "\n";
+#if RUN_IN_DAEMON
+  std::cout << " -d:\n";
+  std::cout << " --daemon:  守护进程运行本程序adbauto\n";
+  std::cout << "\n";
+#endif
+  std::cout << " -v:\n";
+  std::cout << " -verbose:  显示一些冗余信息, 例如: 打印触发的命令\n";
+  std::cout << "\n";
+  std::cout << " -V:\n";
+  std::cout << " --version: 显示版本信息\n";
+  std::cout << "\n";
+  std::cout << "examples:\n";
+  std::cout << " adbauto notify-send 'android device online: {}'\n";
+  std::cout << " adbauto adb -s {} shell setprop name value\n";
+  std::cout << " adbauto adb -s {} uninstall com.UCMobile.mytest\n";
+  std::cout << " adbauto scrcpy -s {}\n";
+  std::cout << " adbauto /path/to/do_somthing_script.sh {}\n";
+  std::cout << "\n";
+  std::cout << " >>> do_somthing_script.sh<<<\n";
+  std::cout << " 1 #!/usr/bin/env bash\n";
+  std::cout << " 2 android_device=\"$1\"\n";
+  std::cout << " 3 adb -s $android_device shell rm /data/local/tmp/mylog.txt\n";
+  std::cout << " 4 adb -s $android_device shell setprop delay 250\n";
+  std::cout << " 5 notify-send \"android device online: $android_device\" # 桌面通知Android连上\n";
+  std::cout << " 6 scrcpy -s $android_device # 桌面投屏显示Android手机截面\n";
+  std::cout << std::endl;
+
+}
+
+void print_version() {
+  std::cout << "0.0.1" << std::endl;
+}
+
+void print_vector_string(std::ostream& out, std::vector<std::string> const& vstrs) {
+  out << "[";
+  for (auto const& str: vstrs) {
+    out << str << ", ";
+  }
+  out << "]" << std::endl;
+}
 
 bool isnewline(char x) { return x == '\n' || x == '\r'; }
 
@@ -29,12 +92,34 @@ void execute_process_for_device(std::vector<char> data, std::vector<std::string>
           if (commands.size() == 0) {
             std::cout << serial << " is online!" << std::endl;
           } else {
-            std::vector<std::string> replace_commands;
+            std::vector<std::string> replace_arguments;
             for (auto i = std::next(commands.begin(), 1); i != commands.end(); ++i) {
-              replace_commands.push_back(boost::algorithm::replace_all_copy(*i, "{}", serial));
+              replace_arguments.push_back(boost::algorithm::replace_all_copy(*i, "{}", serial));
             }
-            boost::process::child child(boost::process::search_path(commands[0]), boost::process::args=replace_commands);
-            child.detach();
+            std::string command = boost::algorithm::replace_all_copy(commands[0], "{}", serial);
+
+            boost::filesystem::path cmd("");
+            if (command.find('/') != std::string::npos) {
+              cmd = boost::filesystem::path(command);
+              if (g_verbose) { std::cout << cmd << " "; print_vector_string(std::cout, replace_arguments); }
+              boost::process::child child(cmd, boost::process::args=replace_arguments);
+              child.detach();
+            } else {
+              cmd = boost::process::search_path(command);
+              if (!exists(cmd) || command.find('=') != std::string::npos) {
+                std::string shell_str = command;
+                for (auto const& i: replace_arguments) {
+                  shell_str = shell_str + " " + i;
+                }
+                if (g_verbose) { std::cout << "bash -c '" << shell_str << "'" << std::endl; }
+                boost::process::child child(boost::process::search_path("bash"), "-c", boost::process::args+=shell_str);
+                child.detach();
+              } else {
+                if (g_verbose) { std::cout << cmd << " "; print_vector_string(std::cout, replace_arguments); }
+                boost::process::child child(cmd, boost::process::args=replace_arguments);
+                child.detach();
+              }
+            }
           }
         }
       }
@@ -97,10 +182,88 @@ void TrackAndroidDevice(int port, std::vector<std::string> const& commands) {
   }
 }
 
-int main(int argc, const char* argv[]) {
+int main(int argc, char* const argv[]) {
+
+  int command_index = 1;
+  for (; command_index < argc; ++command_index) {
+    std::string_view arg(argv[command_index]);
+    if (arg == "-h" || arg == "--help") {
+      usage();
+      exit(0);
+#if RUN_IN_DAEMON
+    } else if (arg == "-d" || arg == "--daemon") {
+      g_daemon_process = true;
+    } else if (arg == "--no-daemon") {
+      g_daemon_process = false;
+#endif
+    } else if (arg == "-v" || arg == "--verbose") {
+      g_verbose = true;
+    } else if (arg == "-V" || arg == "--version") {
+      print_version();
+      exit(0);
+    } else if (arg == "--") {
+      ++command_index;
+      break;
+    } else if (arg[0] == '-') {
+      die(1, std::string("unknown option: ") + arg.data());
+    } else {
+      break;
+    }
+  }
+
+#if RUN_IN_DAEMON
+  pid_t pid;
+  struct sigaction sa;
+  struct rlimit rl;
+  int fd0, fd1, fd2;
+
+
+  if (g_daemon_process) {
+    umask(0);
+
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+      die(1, "can't get file limit.");
+    }
+
+    if ((pid = fork()) < 0) {
+      die(1, "can't fork");
+    } else if (pid != 0) {
+      exit(0);
+    }
+
+    setsid();
+
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGHUP, &sa, NULL) < 0) {
+      die(1, "can't ignore SIGHUP");
+    }
+
+    if ((pid = fork()) < 0) {
+      die(1, "can't fork");
+    } else if (pid != 0) {
+      exit(0);
+    }
+
+    // chdir("/");
+
+    if (rl.rlim_max == RLIM_INFINITY) {
+      rl.rlim_max = 1024;
+    }
+    for (int i = 0; i < rl.rlim_max; ++i) {
+      close(i);
+    }
+
+    fd0 = open("/dev/null", O_RDWR);
+    fd1 = dup(0);
+    fd2 = dup(0);
+  }
+#endif
+
   std::vector<std::string> commands;
-  if (argc >= 2) {
-    commands.insert(commands.end(), argv +1, argv + argc);
+  if (command_index < argc) {
+    commands.insert(commands.end(), argv + command_index, argv + argc);
   }
   TrackAndroidDevice(5037, commands);
   return 0;
